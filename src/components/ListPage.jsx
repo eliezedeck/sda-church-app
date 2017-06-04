69bce8caaf87fc3f21a849756b51902c6b5df27e
Elie Zedeck <elie.zedeck@gmail.com>
2017-06-04 10:57:37 +0300

    Play with subscription. It's not as mature yet ...
    There is an error when an actual data returns from the subscription:
     - WebSocket.client.onmessage (client.js:215)

--- src/components/ListPage.jsx
+++ src/components/ListPage.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
@@ -7,8 +8,32 @@ import gql from 'graphql-tag'
 
 
 class ListPage extends Component {
+  componentWillReceiveProps(newProps) {
+    if (!newProps.posts.loading) {
+      if (this.subscription) {
+        if (newProps.posts !== this.props.posts) {
+          // changed, we need to unsubscribe before resubscribing
+          this.subscription.unsubscribe()
+        } else {
+          // we already have an active subscription with the right params
+          return
+        }
+      }
+
+      this.subscription = newProps.posts.subscribeToMore({
+        document: POSTS_SUBSCRIPTION,
+        updateQuery: (previousResult, { subscriptionData }) => {
+          console.log(subscriptionData)
+          const newResult = _.cloneDeep(previousResult); // never mutate state!
+          return newResult
+        },
+        onError: (err) => console.error(err),
+      })
+    }
+  }
+  
   render() {
-    if (this.props.data.loading) {
+    if (this.props.posts.loading) {
       return (<div>Loading</div>)
     }
 
@@ -18,7 +43,7 @@ class ListPage extends Component {
           + New Post
         </Link>
         <div className='w-100' style={{ maxWidth: 400 }}>
-          {this.props.data.allPosts.map((post) =>
+          {this.props.posts.allPosts.map((post) =>
             <Post key={post.id} post={post} refresh={() => this.props.data.refetch()} />
           )}
         </div>
@@ -27,7 +52,7 @@ class ListPage extends Component {
   }
 }
 
-const FeedQuery = gql`query allPosts {
+const POST_QUERY = gql`query allPosts {
   allPosts(orderBy: createdAt_DESC) {
     id
     imageUrl
@@ -35,4 +60,20 @@ const FeedQuery = gql`query allPosts {
   }
 }`
 
-export default graphql(FeedQuery)(ListPage)
+const POSTS_SUBSCRIPTION = gql`subscription {
+  Post {
+    mutation
+    node {
+      id
+      imageUrl
+      description
+    }
+  }
+} 
+`
+
+const withData = graphql(POST_QUERY, {
+  name: 'posts'
+})
+
+export default withData(ListPage)
