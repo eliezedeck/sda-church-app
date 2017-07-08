c2ca6f82b805b716e40888c538f0071d17b981ad
Elie Zedeck <elie.zedeck@gmail.com>
2017-07-08 07:17:49 +0300

    Update the prayerRequests logic

--- meteor/server/logics/prayersRequests.js
+++ meteor/server/logics/prayersRequests.js
@@ -9,21 +9,27 @@ import SimpleSchema from 'simpl-schema'
  */
 
 const documentSchema = new SimpleSchema({
-  name: {
+  content: {
     type: String,
-    min: 2,
-    max: 40
+    min: 4,
+    max: 4096
   },
 
-  code: {
+  createdBy: {
     type: String,
-    min: 1,
-    max: 40,
-    index: true,
-    unique: true,
+    min: 17,
+    max: 17,
+    optional: true,
+    denyUpdate: true
   },
 
-  articlesCount: {
+  createdAt: {
+    type: Date,
+    optional: true,
+    index: true, // index so we can sort by the most recent prayer request
+  },
+
+  viewsCount: {
     type: SimpleSchema.Integer,
     min: 0,
     optional: true
@@ -48,21 +54,23 @@ PrayerRequestsCollection.attachSchema(documentSchema, {transform: false})
  */
 
 new ValidatedMethod({
-  name: 'suppliers.add',
+  name: 'prayerRequests.add',
   validate: new SimpleSchema({
     doc: {
       type: documentSchema
     }
   }).validator(),
   run({ doc }) {
-    if (!Roles.userIsInRole(this.userId, ['owner']))
-      throw new Meteor.Error(403, "Vous n'êtes pas autorisé!")
+    if (!Roles.userIsInRole(this.userId, ['member']))
+      throw new Meteor.Error(403, "Not authorized. Please ask an administrator to validate who you are.")
 
     // Override
-    doc.articlesCount = 0
+    doc.viewsCount = 0
+    doc.createdBy = this.userId
+    doc.createdAt = new Date()
 
     try {
-      return PrayerRequestsCollection.insert(doc)
+      return PrayerRequestsCollection.insertOne(doc)
     } catch (e) {
       throw new Meteor.Error(400, e.message)
     }
@@ -70,7 +78,7 @@ new ValidatedMethod({
 })
 
 new ValidatedMethod({
-  name: 'suppliers.update',
+  name: 'prayerRequests.update',
   validate: new SimpleSchema({
     id: {
       type: String,
@@ -82,15 +90,22 @@ new ValidatedMethod({
     }
   }).validator(),
   run({ id, doc }) {
-    if (!Roles.userIsInRole(this.userId, ['owner']))
-      throw new Meteor.Error(403, "Vous n'êtes pas autorisé!")
+    if (!Roles.userIsInRole(this.userId, ['member']))
+      throw new Meteor.Error(403, "Not authorized. Please ask an administrator to validate who you are.")
+    const pr = PrayerRequestsCollection.findOne(id)
+    if (!pr)
+      throw new Meteor.Error(400, "Prayer request not found")
+    if (this.userId !== pr.createdBy && !Roles.userIsInRole(this.userId, ['admin', 'zedeck']))
+      throw new Meteor.Error(403, "Not authorized. Prayer request doesn't belong to you.")
 
     // Override
     delete doc._id
-    delete doc.articlesCount
+    delete doc.viewsCount
+    delete doc.createdBy
+    delete doc.createdAt
 
     try {
-      return PrayerRequestsCollection.update(id, {
+      return PrayerRequestsCollection.updateOne(id, {
         $set: doc
       })
     } catch (e) {
@@ -115,7 +130,7 @@ new ValidatedMethod({
  */
 
 Meteor.publish('prayerRequests.all', function () { //
-  if (Roles.userIsInRole(this.userId, ['member'])) {
+  if (Roles.userIsInRole(this.userId, ['member', 'admin', 'zedeck'])) {
     return PrayerRequestsCollection.find({})
   }
 
