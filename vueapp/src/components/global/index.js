d25c5268243ded4d4210d36048316d66abdb7e9d
Elie Zedeck <elie.zedeck@gmail.com>
2017-07-08 21:37:20 +0300

    Add a global 'loading' component

--- vueapp/src/components/global/index.js
+++ vueapp/src/components/global/index.js
@@ -6,3 +6,8 @@ import SubmitButton from './SubmitButton.vue'
 // Register the global components
 Vue.component('error-alert', ErrorAlert)
 Vue.component('submit-button', SubmitButton)
+
+Vue.component('loading', {
+  /* TAG: i18n */
+  template: '<span><i class="glyphicon glyphicon-refresh spin"></i> <slot>Loading ...</slot></span>'
+})
