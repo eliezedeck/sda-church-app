6494094a930788136dd9047dd297b81082c6b597
Elie Zedeck <elie.zedeck@gmail.com>
2017-07-08 21:52:26 +0300

    Render the selected prayer as Markdown

--- vueapp/src/components/PrayerRequests.vue
+++ vueapp/src/components/PrayerRequests.vue
@@ -10,62 +10,65 @@
         </div>
         <PrayerRequestForm v-if="showPrayerForm" @dismiss="showPrayerForm = false"></PrayerRequestForm>
 
-        <h4>
-          <button class="btn btn-warning btn-xs" type="button"><i class="glyphicon glyphicon-pencil"></i> Edit</button> &mdash; Request by ... - <small>Text</small></h4>
-        <p>Details and Contents of the request</p>
-        <hr />
-        <p class="help-block" style="margin-top: 0">Do you think that this is not a Prayer request or it contains inappropriate contents? Help use to moderate the content of this Website by <a href="#"><i class="glyphicon glyphicon-flag"></i> flagging</a> this request.</p>
-        <div role="group"
-             class="btn-group" style="margin-bottom: 1em">
-          <button class="btn btn-primary" type="button"><i class="glyphicon glyphicon-plus"></i> Comments </button>
-          <button class="btn btn-default" type="button"><i class="glyphicon glyphicon-plus"></i> Testimony </button>
-          <button class="btn btn-success" type="button"><i class="glyphicon glyphicon-ok"></i> God has answered this Prayer !</button>
-        </div>
-        <div class="well well-sm">
-          <form>
-            <div class="form-group">
-              <label class="control-label">Your testimony</label>
-              <textarea rows="6" class="form-control input-lg"></textarea>
-              <p class="help-block">Text is in &quot;Markdown&quot; notation</p>
-            </div>
-            <div class="checkbox">
-              <label>
-                <input type="checkbox" />Make this Testimony also visible in the Testimonies page</label>
-            </div>
-            <div role="group" class="btn-group">
-              <button class="btn btn-primary" type="button">Add my Testimony</button>
-              <button class="btn btn-default" type="button">Cancel </button>
-            </div>
-          </form>
-        </div>
-        <div class="well well-sm">
-          <form>
-            <div class="form-group">
-              <label class="control-label">Your comments</label>
-              <textarea rows="4" placeholder="Encourage, give information, ..." class="form-control"></textarea>
-              <p class="help-block">Text is in &quot;Markdown&quot; notation</p>
-            </div>
-            <div role="group" class="btn-group">
-              <button class="btn btn-primary" type="button">Add my Comments</button>
-              <button class="btn btn-default" type="button">Cancel </button>
-            </div>
-          </form>
-        </div>
-        <p>I would like to <a href="#">Pray</a> for this request. Thank you for supporting Tiana by praying for his request.</p>
-        <div class="table-responsive">
-          <table class="table table-striped">
-            <tbody>
-            <tr>
-              <td> <a href="#">Delete this comment</a></td>
-              <td>Cell 2</td>
-            </tr>
-            <tr>
-              <td>By sdfjklm</td>
-              <td>Cell 4</td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
+        <template v-if="selectedPrayerRequestObj">
+          <h4>
+            <button class="btn btn-warning btn-xs" type="button"><i class="glyphicon glyphicon-pencil"></i> Edit</button> &mdash; Request by ...</h4>
+          <div v-html="marked(selectedPrayerRequestObj.content)"></div>
+
+          <hr />
+          <p class="help-block" style="margin-top: 0">Do you think that this is not a Prayer request or it contains inappropriate contents? Help use to moderate the content of this Website by <a href="#"><i class="glyphicon glyphicon-flag"></i> flagging</a> this request.</p>
+          <div role="group"
+               class="btn-group" style="margin-bottom: 1em">
+            <button class="btn btn-primary" type="button"><i class="glyphicon glyphicon-plus"></i> Comments </button>
+            <button class="btn btn-default" type="button"><i class="glyphicon glyphicon-plus"></i> Testimony </button>
+            <button class="btn btn-success" type="button"><i class="glyphicon glyphicon-ok"></i> God has answered this Prayer !</button>
+          </div>
+          <div class="well well-sm">
+            <form>
+              <div class="form-group">
+                <label class="control-label">Your testimony</label>
+                <textarea rows="6" class="form-control input-lg"></textarea>
+                <p class="help-block">Text is in &quot;Markdown&quot; notation</p>
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" />Make this Testimony also visible in the Testimonies page</label>
+              </div>
+              <div role="group" class="btn-group">
+                <button class="btn btn-primary" type="button">Add my Testimony</button>
+                <button class="btn btn-default" type="button">Cancel </button>
+              </div>
+            </form>
+          </div>
+          <div class="well well-sm">
+            <form>
+              <div class="form-group">
+                <label class="control-label">Your comments</label>
+                <textarea rows="4" placeholder="Encourage, give information, ..." class="form-control"></textarea>
+                <p class="help-block">Text is in &quot;Markdown&quot; notation</p>
+              </div>
+              <div role="group" class="btn-group">
+                <button class="btn btn-primary" type="button">Add my Comments</button>
+                <button class="btn btn-default" type="button">Cancel </button>
+              </div>
+            </form>
+          </div>
+          <p>I would like to <a href="#">Pray</a> for this request. Thank you for supporting Tiana by praying for his request.</p>
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <tbody>
+              <tr>
+                <td> <a href="#">Delete this comment</a></td>
+                <td>Cell 2</td>
+              </tr>
+              <tr>
+                <td>By sdfjklm</td>
+                <td>Cell 4</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </template>
       </div>
       <div class="col-md-5">
         <PrayerRequestsList
@@ -83,9 +86,12 @@
   import PrayerRequestsCollection from 'server/collections/prayerRequests'
 
   import List from './lists/PrayerRequestsList.vue'
+  import {MarkedMixin} from '../data/marked.js'
 
   export default {
     name: 'PrayerRequests',
+
+    mixins: [MarkedMixin],
 
     data() {
       return {
@@ -101,6 +107,16 @@
       prayerRequests() {
         // Inherit the sorting order by the server
         return PrayerRequestsCollection.find()
+      },
+
+      selectedPrayerRequestObj: {
+        params() {
+          return {id: this.$route.params['selectedId']}
+        },
+
+        update({id}) {
+          return PrayerRequestsCollection.findOne(id)
+        }
       }
     },
 
