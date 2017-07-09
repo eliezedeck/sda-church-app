99ca59e47ad784a2b453609235ddc27a636177b2
Elie Zedeck <elie.zedeck@gmail.com>
2017-07-09 08:59:36 +0300

    Remove the 'admin' role

--- vueapp/src/data/roles.js
+++ vueapp/src/data/roles.js
@@ -2,7 +2,6 @@
 // TODO: move this to Meteor domain and enforce the profile
 export const roles = {
   'zedeck': 'Zedeck',
-  'admin': 'Administrator',
   'church-clerk': 'Church Clerk',
   'member': 'Member'
 }
