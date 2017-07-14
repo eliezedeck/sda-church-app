7e81de4a05795b8668603dfc208d6c0459048fa9
Elie Zedeck <elie.zedeck@gmail.com>
2017-07-14 21:24:33 +0300

    Import the update from Mendrika for the data/form mixin

--- vueapp/src/data/form.js
+++ vueapp/src/data/form.js
@@ -2,14 +2,16 @@ import _ from 'lodash'
 import {Meteor} from 'meteor/meteor'
 
 
-export const getFormHelperMixin = ({base, overwritePropsKey, insertMethod, updateMethod}) => {
+export const getFormHelperMixin = ({base, overwritePropsKey, insertMethod, updateMethod, insertDataModifier}) => {
   // Prepare the watch property
   const watch = {}
-  watch[overwritePropsKey] = 'dataAssign'
+  if (overwritePropsKey)
+    watch[overwritePropsKey] = 'dataAssign'
 
   return {
     data() {
-      return _.assign(this.dataAssign(), {
+      const state = this.dataAssign()
+      return _.assign(_.pickBy(state, s => s !== undefined), {
         inProgress: false,
         error: null
       })
@@ -18,9 +20,21 @@ export const getFormHelperMixin = ({base, overwritePropsKey, insertMethod, updat
     watch,
 
     methods: {
+      _dataAssignBase() {
+        const source = {}
+        _.forEach(base, (v, k) => {
+          if (typeof v === 'function')
+            source[k] = v.bind(this)()
+          else
+            source[k] = _.clone(v)
+        })
+        return source
+      },
+
       dataAssign() {
-        const source = _.cloneDeep(base) // make sure to preserve the original base
-        const overwrite = _.get(this, overwritePropsKey, {})
+        // Execute computed properties for the base
+        const source = this._dataAssignBase()
+        const overwrite = overwritePropsKey ? _.get(this, overwritePropsKey, {}) : {}
         const state = _.keys(overwrite).length > 0 ? _.assign(source, overwrite) : source
 
         if (this._dataAssignInitialized) {
@@ -40,16 +54,16 @@ export const getFormHelperMixin = ({base, overwritePropsKey, insertMethod, updat
 
         // Reconstruct the actual data that we need
         let data = {}
-        _.forEach(base, (v, key) => {
+        _.forEach(this._dataAssignBase(), (v, key) => {
           data[key] = this[key]
         })
 
-        const callback = (err) => {
+        const callback = (err, response) => {
           this.inProgress = false
           if (err) {
             this.error = err.message
           } else {
-            this.$emit('dismiss')
+            this.$emit('dismiss', response)
           }
         }
 
@@ -65,9 +79,11 @@ export const getFormHelperMixin = ({base, overwritePropsKey, insertMethod, updat
         else {
           // Inserting
           if (insertMethod) {
-            Meteor.call(insertMethod, {
-              doc: data
-            }, callback)
+            const d = {doc: data}
+            if (typeof insertDataModifier === 'function')
+              insertDataModifier.bind(this)(d)
+
+            Meteor.call(insertMethod, d, callback)
           }
         }
       }
