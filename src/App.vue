a092231d19745461a0559fba3ebc4d252755e613
Elie Zedeck RANDRIAMIANDRIRAY <elie.zedeck@gmail.com>
2017-09-19 20:59:40 +0300

    Drop the use of the Bootstrap Studio theme files and codes

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
     </div>
 
     <div>
-      <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean">
+      <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container">
           <div class="navbar-header"><a href="#" class="navbar-brand navbar-link">ESC of Antananarivo</a>
             <button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggle collapsed"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
@@ -65,37 +65,6 @@
 
     <div id="contents" style="min-height: 38em;">
       <router-view></router-view>
-    </div>
-
-    <div class="footer-dark">
-      <footer>
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6 col-md-push-6 item text">
-              <h3>Company Name</h3>
-              <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
-            </div>
-            <div class="col-md-3 col-md-pull-6 col-sm-4 item">
-              <h3>Services</h3>
-              <ul>
-                <li><a href="#">Web design</a></li>
-                <li><a href="#">Development</a></li>
-                <li><a href="#">Hosting</a></li>
-              </ul>
-            </div>
-            <div class="col-md-3 col-md-pull-6 col-sm-4 item">
-              <h3>About</h3>
-              <ul>
-                <li><a href="#">Company</a></li>
-                <li><a href="#">Team</a></li>
-                <li><a href="#">Careers</a></li>
-              </ul>
-            </div>
-            <div class="col-md-12 col-sm-4 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
-          </div>
-          <p class="copyright">English Speaking Church of ANTANANARIVO © 2017 &mdash; An application by <a href="http://www.eliezedeck.com" target="_blank">ElieZedeck.com </a></p>
-        </div>
-      </footer>
     </div>
   </div>
 </template>
