d0542ea09152770252e4b25ef75aea16bc113d1f
Elie Zedeck RANDRIAMIANDRIRAY <elie.zedeck@gmail.com>
2018-02-16 23:32:38 +0300

    Connected to Baqend server

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <div v-if="!baqendReady">
+      Connecting to server ...
+    </div>
+
+    <router-view v-else></router-view>
+  </div>
 </template>
 
 <script>
-  import 'bootstrap/dist/js/bootstrap'
-  import 'bootstrap/dist/css/bootstrap.css'
-  import 'typeface-abel'
-  import '@fortawesome/fontawesome'
-  import '@fortawesome/fontawesome-free-solid'
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'typeface-abel'
+import '@fortawesome/fontawesome'
+import '@fortawesome/fontawesome-free-solid'
 
-  export default {
-    name: 'APP'
+import DB from 'baqend/realtime'
+
+export default {
+  name: 'APP',
+
+  data () {
+    return {
+      baqendReady: false
+    }
+  },
+
+  mounted () {
+    // Deduce the URL to Baqend local server
+    const loc = window.location
+    let connectTarget = `${loc.protocol}//${loc.hostname}:${loc.port}/v1`
+    if (loc.hostname === 'localhost') {
+      connectTarget = `${loc.protocol}//localhost:2480/v1`
+      console.log(`Targetting server ${connectTarget} ...`)
+    }
+
+    DB.connect(connectTarget)
+    DB.ready(() => {
+      this.baqendReady = true
+      console.log('Connected to server.')
+    })
   }
+}
 </script>
 
 <style>
