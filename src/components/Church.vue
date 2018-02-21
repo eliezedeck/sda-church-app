4230c11abdd73eac6c5f933bb248b401bfc671e3
Elie Zedeck RANDRIAMIANDRIRAY <elie.zedeck@gmail.com>
2018-02-21 21:11:56 +0300

    Add stub AccountForm

--- src/components/Church.vue
+++ src/components/Church.vue
@@ -5,7 +5,7 @@
         <h1 style="margin-top: 1rem">Welcome</h1>
       </div>
 
-      <div v-if="$route.name === 'Church'" class="col-12">
+      <div v-if="$route.name === 'Church'" class="col-6">
         <div v-if="!showDepartmentForm" role="group" class="btn-group">
           <button @click="showDepartmentForm = true" class="btn btn-primary" type="button"><i class="fa fa-plus"></i> Add a Department</button>
         </div>
@@ -27,6 +27,31 @@
           </table>
         </div>
       </div>
+
+      <div v-if="$route.name === 'Church'" class="col-6">
+        <AccountForm v-if="showAccountForm" @dismissed="showAccountForm = false"></AccountForm>
+        <div v-else role="group" class="btn-group">
+          <button @click="showAccountForm = true" class="btn btn-primary" type="button"><i class="fa fa-plus"></i> Add an <em>Unlinked</em> account</button>
+        </div>
+
+        <div class="table-responsive" style="margin-top: 1rem">
+          <table class="table table-bordered table-hover">
+            <thead>
+            <tr>
+              <th>Account name</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>Cell 1</td>
+            </tr>
+            <tr>
+              <td>Cell 3</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
 
     <router-view v-if="$route.name === 'Department' && departments !== {}" :department="departments[`department/${$route.params.departmentId}`]"></router-view>
@@ -35,13 +60,15 @@
 
 <script>
 import DepartmentForm from './forms/DepartmentForm'
+import AccountForm from './forms/AccountForm'
 
 export default {
   name: 'Church',
 
   data () {
     return {
-      showDepartmentForm: false
+      showDepartmentForm: false,
+      showAccountForm: false
     }
   },
 
@@ -58,7 +85,8 @@ export default {
   },
 
   components: {
-    DepartmentForm
+    DepartmentForm,
+    AccountForm
   }
 }
 </script>
---
+++ src/components/forms/AccountForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <form @submit.prevent.stop="saveAccount()">
+    <div class="card text-light bg-dark">
+      <div class="card-body">
+        <h4 class="card-title">Add an account</h4>
+        <div class="form-group">
+          <label>Account name</label>
+          <input v-model="form.name" type="text" autofocus autocomplete="off" class="form-control" />
+        </div>
+        <div class="form-check">
+          <input v-model="form.isDepartmentAccount" :true-value="false" :false-value="true" disabled type="checkbox" class="form-check-input" id="formCheck-1" />
+          <label for="formCheck-1" class="form-check-label">Account is not linked to a department</label>
+        </div>
+        <div role="group" class="btn-group" style="margin-top: 1rem">
+          <button class="btn btn-primary" type="submit">Save</button>
+          <button @click="$emit('dismissed')" class="btn btn-secondary" type="button">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AccountForm',
+
+  data () {
+    return {
+      form: {
+        name: '',
+        isDepartmentAccount: false
+      }
+    }
+  },
+
+  methods: {
+    saveAccount () {
+      this.$emit('dismissed')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
